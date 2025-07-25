2b261b5e7c8125d45aed8432f1a19c2ad683b22d
Jarret Diep <jarrettcdi@gmail.com>
2025-07-24 22:23:46 -0600

    buffer simple cube vertices

--- main.cpp
+++ main.cpp
@@ -44,6 +44,40 @@ void initialize() {
 	glfwSetFramebufferSizeCallback(window, &frame_buffer_size_callback);
 }
 
+const float cubeVertices[] = {
+	// front face
+	-0.5, -0.5, 0.5, // bottom left
+	-0.5,  0.5, 0.5, // top left
+	 0.5,  0.5, 0.5, // top right
+	 0.5, -0.5, 0.5, // bottom right
+	// back face
+	-0.5, -0.5, -0.5,
+	-0.5,  0.5, -0.5,
+	 0.5, -0.5, -0.5,
+	 0.5,  0.5, -0.5,
+};
+
+const int cubeIndices[] = {
+	// front face
+	0, 1, 2, // top left
+	0, 2, 3, // top right
+	// top face
+	1, 2, 5,
+	2, 5, 6,
+	// right face
+	2, 3, 6,
+	3, 6, 7,
+	// bottom face
+	0, 3, 4,
+	3, 4, 7,
+	// left face
+	0, 1, 4,
+	1, 4, 5,
+	// back face
+	4, 5, 6,
+	4, 6, 7
+};
+
 int main() {
 	try {
 		initialize();
@@ -53,8 +87,28 @@ int main() {
 
 	Shader blockShader("shader.vs", "shader.fs");
 
+	unsigned int cubeVAO, cubeVBO, cubeEBO;
+
+	glGenVertexArrays(1, &cubeVAO);
+	glGenBuffers(1, &cubeVBO);
+	glGenBuffers(1, &cubeEBO);
+
+	glBindVertexArray(cubeVAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeEBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+	glEnableVertexAttribArray(0);
+
 	while (!glfwWindowShouldClose(window)) {
 		process_input(window);
+		
+		glClearColor(0.3, 0.75, 0.85, 1.0);
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
