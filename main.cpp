85709264ec7c89f28d7b750c5e50a7cee5964eef
Jarret Diep <jarrettcdi@gmail.com>
2025-07-24 23:00:00 -0600

    extract buffering, drew first cube

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
 #include <exception>
 
 #include "shader.h"
 #include "camera.h"
+
+using glm::vec3;
+using glm::mat4;
 
 constexpr int WIDTH = 800;
 constexpr int HEIGHT = 600;
@@ -53,42 +59,32 @@ const float cubeVertices[] = {
 	// back face
 	-0.5, -0.5, -0.5,
 	-0.5,  0.5, -0.5,
-	 0.5, -0.5, -0.5,
 	 0.5,  0.5, -0.5,
+	 0.5, -0.5, -0.5,
 };
 
 const int cubeIndices[] = {
 	// front face
-	0, 1, 2, // top left
-	0, 2, 3, // top right
+	0, 1, 2,
+	0, 2, 3,
 	// top face
-	1, 2, 5,
-	2, 5, 6,
+	1, 5, 6,
+	1, 6, 2,
 	// right face
-	2, 3, 6,
-	3, 6, 7,
+	2, 3, 7,
+	2, 7, 6,
 	// bottom face
-	0, 3, 4,
-	3, 4, 7,
+	0, 4, 7,
+	0, 7, 3,
 	// left face
-	0, 1, 4,
-	1, 4, 5,
+	0, 1, 5,
+	0, 5, 4,
 	// back face
 	4, 5, 6,
 	4, 6, 7
 };
 
-int main() {
-	try {
-		initialize();
-	} catch (std::exception& e) {
-		std::cout << e.what() << std::endl;
-	}
-
-	Shader blockShader("shader.vs", "shader.fs");
-
-	unsigned int cubeVAO, cubeVBO, cubeEBO;
-
+void bufferCubeData(unsigned int & cubeVAO, unsigned int& cubeVBO, unsigned int& cubeEBO) {
 	glGenVertexArrays(1, &cubeVAO);
 	glGenBuffers(1, &cubeVBO);
 	glGenBuffers(1, &cubeEBO);
@@ -101,8 +97,24 @@ int main() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubeEBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+}
+
+int main() {
+	try {
+		initialize();
+	} catch (std::exception& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	unsigned int cubeVAO, cubeVBO, cubeEBO;
+	bufferCubeData(cubeVAO, cubeVBO, cubeEBO);
+
+	Shader blockShader("shader.vs", "shader.fs");
+	Camera camera(vec3(0.0f, 0.0f, 3.0f));
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	while (!glfwWindowShouldClose(window)) {
 		process_input(window);
@@ -110,10 +122,27 @@ int main() {
 		glClearColor(0.3, 0.75, 0.85, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		blockShader.use();
+		mat4 model(1.0f);
+		mat4 view = camera.GetViewMatrix();
+		mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+		blockShader.setMat4("model", model);
+		blockShader.setMat4("view", view);
+		blockShader.setMat4("projection", projection);
+
+		blockShader.setVec3("vertexColour", vec3(0.25, 0.85, 0.3));
+
+		glBindVertexArray(cubeVAO);
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	glDeleteVertexArrays(1, &cubeVAO);
+	glDeleteBuffers(1, &cubeVBO);
+	glDeleteBuffers(1, &cubeEBO);
+	
 	glfwTerminate();
 	return 0;
 }
