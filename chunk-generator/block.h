d920cafbe881d3bb0a4abc7f6c066290e5e5aeab
Jarret Diep <jarrettcdi@gmail.com>
2025-08-23 17:51:12 -0600

    Abstract and refactor Block for scalability
    -add Block namespace (holds all the below)
    -change BlockType from enum to int
    -add BlockTag enum, will be used for logic later
    -add BlockDef struct, holds metadata for each block type (like bitset of BlockType)
    -add BlockRegistry, which maps a BlockType to a BlockDef

--- chunk-generator/block.h
+++ chunk-generator/block.h
@@ -2,63 +2,116 @@
 
 #include <glm/glm.hpp>
 
+#include <bitset>
+#include <string>
+
 #include "mesh.h"
 
 using glm::vec3;
 
-enum class BlockType : uint8_t {
-	AIR = 0,
-	GRASS = 1,
-	STONE = 2,
-};
+namespace Block {
+    using BlockType = uint8_t;
 
-// Cube vertices, 6 faces × 2 triangles × 3 vertices = 36 vertices
-const Vertex cubeVertices[] = {
-    // --- Front face ---
-    {{-0.5f, -0.5f,  0.5f}, {0, 0, 1}, {0,0}},
-    {{ 0.5f, -0.5f,  0.5f}, {0, 0, 1}, {1,0}},
-    {{ 0.5f,  0.5f,  0.5f}, {0, 0, 1}, {1,1}},
-    {{ 0.5f,  0.5f,  0.5f}, {0, 0, 1}, {1,1}},
-    {{-0.5f,  0.5f,  0.5f}, {0, 0, 1}, {0,1}},
-    {{-0.5f, -0.5f,  0.5f}, {0, 0, 1}, {0,0}},
+    // Maintain the BlockTag and tagToString together
+    enum class BlockTag : uint8_t {
+        Invisible,
+        Transparent,
 
-    // --- Back face ---
-    {{ 0.5f, -0.5f, -0.5f}, {0, 0, -1}, {0,0}},
-    {{-0.5f, -0.5f, -0.5f}, {0, 0, -1}, {1,0}},
-    {{-0.5f,  0.5f, -0.5f}, {0, 0, -1}, {1,1}},
-    {{-0.5f,  0.5f, -0.5f}, {0, 0, -1}, {1,1}},
-    {{ 0.5f,  0.5f, -0.5f}, {0, 0, -1}, {0,1}},
-    {{ 0.5f, -0.5f, -0.5f}, {0, 0, -1}, {0,0}},
+        COUNT, // used to easily get the length of the enum
+    };
 
-    // --- Left face ---
-    {{-0.5f, -0.5f, -0.5f}, {-1,0,0}, {0,0}},
-    {{-0.5f, -0.5f,  0.5f}, {-1,0,0}, {1,0}},
-    {{-0.5f,  0.5f,  0.5f}, {-1,0,0}, {1,1}},
-    {{-0.5f,  0.5f,  0.5f}, {-1,0,0}, {1,1}},
-    {{-0.5f,  0.5f, -0.5f}, {-1,0,0}, {0,1}},
-    {{-0.5f, -0.5f, -0.5f}, {-1,0,0}, {0,0}},
+    inline std::string tagToString(BlockTag tag) {
+        switch (tag) {
+            case BlockTag::Invisible:   
+                return "Invisible";
+            case BlockTag::Transparent: 
+                return "Transparent";
+            default:                    
+                return "Unknown";
+        }
+    }
 
-    // --- Right face ---
-    {{ 0.5f, -0.5f,  0.5f}, {1,0,0}, {0,0}},
-    {{ 0.5f, -0.5f, -0.5f}, {1,0,0}, {1,0}},
-    {{ 0.5f,  0.5f, -0.5f}, {1,0,0}, {1,1}},
-    {{ 0.5f,  0.5f, -0.5f}, {1,0,0}, {1,1}},
-    {{ 0.5f,  0.5f,  0.5f}, {1,0,0}, {0,1}},
-    {{ 0.5f, -0.5f,  0.5f}, {1,0,0}, {0,0}},
+    struct BlockDef {
+        std::string name;
+        int textureIndices[6]{};
+        std::bitset<static_cast<size_t>(BlockTag::COUNT)> tags;
 
-    // --- Top face ---
-    {{-0.5f, 0.5f,  0.5f}, {0,1,0}, {0,0}},
-    {{ 0.5f, 0.5f,  0.5f}, {0,1,0}, {1,0}},
-    {{ 0.5f, 0.5f, -0.5f}, {0,1,0}, {1,1}},
-    {{ 0.5f, 0.5f, -0.5f}, {0,1,0}, {1,1}},
-    {{-0.5f, 0.5f, -0.5f}, {0,1,0}, {0,1}},
-    {{-0.5f, 0.5f,  0.5f}, {0,1,0}, {0,0}},
+        inline bool hasTag(BlockTag tag) const {
+            return tags.test(static_cast<size_t>(tag));
+        }
+    };
 
-    // --- Bottom face ---
-    {{-0.5f, -0.5f, -0.5f}, {0,-1,0}, {0,0}},
-    {{ 0.5f, -0.5f, -0.5f}, {0,-1,0}, {1,0}},
-    {{ 0.5f, -0.5f,  0.5f}, {0,-1,0}, {1,1}},
-    {{ 0.5f, -0.5f,  0.5f}, {0,-1,0}, {1,1}},
-    {{-0.5f, -0.5f,  0.5f}, {0,-1,0}, {0,1}},
-    {{-0.5f, -0.5f, -0.5f}, {0,-1,0}, {0,0}}
-};
+    class BlockRegistry { // Singleton
+    public:
+        static BlockRegistry& getInstance() {
+            static BlockRegistry instance;
+            return instance;
+        }
+
+        inline void registerBlock(BlockDef def) {
+            blockDefs[uniqueID++] = std::move(def);
+        }
+
+        inline const BlockDef& get(BlockType type) const {
+            return blockDefs.at(type);
+        }
+
+        void testRegister();
+
+    private:
+        int uniqueID = 0;
+        std::vector<BlockDef> blockDefs;
+        BlockRegistry() = default;
+    };
+
+    // Cube vertices, 6 faces × 2 triangles × 3 vertices = 36 vertices
+    const Vertex cubeVertices[] = {
+        // --- Front face ---
+        {{-0.5f, -0.5f,  0.5f}, {0, 0, 1}, {0,0}},
+        {{ 0.5f, -0.5f,  0.5f}, {0, 0, 1}, {1,0}},
+        {{ 0.5f,  0.5f,  0.5f}, {0, 0, 1}, {1,1}},
+        {{ 0.5f,  0.5f,  0.5f}, {0, 0, 1}, {1,1}},
+        {{-0.5f,  0.5f,  0.5f}, {0, 0, 1}, {0,1}},
+        {{-0.5f, -0.5f,  0.5f}, {0, 0, 1}, {0,0}},
+
+        // --- Back face ---
+        {{ 0.5f, -0.5f, -0.5f}, {0, 0, -1}, {0,0}},
+        {{-0.5f, -0.5f, -0.5f}, {0, 0, -1}, {1,0}},
+        {{-0.5f,  0.5f, -0.5f}, {0, 0, -1}, {1,1}},
+        {{-0.5f,  0.5f, -0.5f}, {0, 0, -1}, {1,1}},
+        {{ 0.5f,  0.5f, -0.5f}, {0, 0, -1}, {0,1}},
+        {{ 0.5f, -0.5f, -0.5f}, {0, 0, -1}, {0,0}},
+
+        // --- Left face ---
+        {{-0.5f, -0.5f, -0.5f}, {-1,0,0}, {0,0}},
+        {{-0.5f, -0.5f,  0.5f}, {-1,0,0}, {1,0}},
+        {{-0.5f,  0.5f,  0.5f}, {-1,0,0}, {1,1}},
+        {{-0.5f,  0.5f,  0.5f}, {-1,0,0}, {1,1}},
+        {{-0.5f,  0.5f, -0.5f}, {-1,0,0}, {0,1}},
+        {{-0.5f, -0.5f, -0.5f}, {-1,0,0}, {0,0}},
+
+        // --- Right face ---
+        {{ 0.5f, -0.5f,  0.5f}, {1,0,0}, {0,0}},
+        {{ 0.5f, -0.5f, -0.5f}, {1,0,0}, {1,0}},
+        {{ 0.5f,  0.5f, -0.5f}, {1,0,0}, {1,1}},
+        {{ 0.5f,  0.5f, -0.5f}, {1,0,0}, {1,1}},
+        {{ 0.5f,  0.5f,  0.5f}, {1,0,0}, {0,1}},
+        {{ 0.5f, -0.5f,  0.5f}, {1,0,0}, {0,0}},
+
+        // --- Top face ---
+        {{-0.5f, 0.5f,  0.5f}, {0,1,0}, {0,0}},
+        {{ 0.5f, 0.5f,  0.5f}, {0,1,0}, {1,0}},
+        {{ 0.5f, 0.5f, -0.5f}, {0,1,0}, {1,1}},
+        {{ 0.5f, 0.5f, -0.5f}, {0,1,0}, {1,1}},
+        {{-0.5f, 0.5f, -0.5f}, {0,1,0}, {0,1}},
+        {{-0.5f, 0.5f,  0.5f}, {0,1,0}, {0,0}},
+
+        // --- Bottom face ---
+        {{-0.5f, -0.5f, -0.5f}, {0,-1,0}, {0,0}},
+        {{ 0.5f, -0.5f, -0.5f}, {0,-1,0}, {1,0}},
+        {{ 0.5f, -0.5f,  0.5f}, {0,-1,0}, {1,1}},
+        {{ 0.5f, -0.5f,  0.5f}, {0,-1,0}, {1,1}},
+        {{-0.5f, -0.5f,  0.5f}, {0,-1,0}, {0,1}},
+        {{-0.5f, -0.5f, -0.5f}, {0,-1,0}, {0,0}}
+    };
+}
